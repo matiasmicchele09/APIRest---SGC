6efa77cac0a062546e9f1142ea67bcbbf5f7096d
Matias Micchele <matias.micchele@hotmail.com>
2025-03-04 11:44:23 -0300

    pequeñas correcciones

--- src/app.js
+++ src/app.js
@@ -3,26 +3,43 @@ import usersRoutes from './routes/users.routes.js'
 import rolesRoutes from './routes/roles.routes.js'
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-
+import jwt from 'jsonwebtoken';
+import { getUserLogIn } from './controllers/users.controllers.js';  
+import { SECRET_JWT_KEY } from './config.js';
 const app = express();
 
 //* Middlewares
 app.use(cors());
 app.use(express.json()); 
 app.use(cookieParser());
+
+
+//TODO: Ver como mejorar esto.
+//TODO: tuve que poner el login aparte, porque ahi hago el jwt, y sino lo que pasaba era que...
+//TODO: ...no tenia el re.cookies.access_token, porque no lo habia creado todavia...
+//TODO: ...y si ponia primero todas las rutas del usuario con el login iba bien, pero despues no me tomaba ekl user, porque no tenia el token porque no lo habia guardado...
+//TODO: ... como lo hago en el middleware de app.js
+//TODO: ¿Y si hago lo del middleware en la ruta del logueo, si guardo ahi la session? no se, no me convece.
+app.post('/login', getUserLogIn);
 app.use((req, res, next) => {
     const token = req.cookies.access_token;
-    let data = null;
+    
+    //Agrego información a la sesión
+    req.session = {user: null};
 
     try {
-        data = jwt.verify(token, SECRET_JWT_KEY);
+       const data = jwt.verify(token, SECRET_JWT_KEY);
         req.session.user = data;
-    } catch (error) {}
+    } catch (error) {
+        res.json({error})
+        //console.log(error);
+    }
     next() //seguir a la siguiente ruta o middleware
-
 });
+
+app.use(usersRoutes);
 //Cada vez que manden un json a la aplicación voy a poder interpretarlo y voy a poder guardarlo dentro de un req.body
 //Así que cada vez que llame al req.body voy a poder utilizar los datos que el cliente me este enviando
 app.use(rolesRoutes);
-app.use(usersRoutes);
+
 export default app;
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,34 +3,20 @@ import { Router } from 'express'
 import { getUsers, getUser, createUsers, updateUser, deleteUser, getUserByRol, getUserLogIn, logOut } from '../controllers/users.controllers.js'
 const router = Router()
 
-router.get('/users', getUsers)
-router.get('/users/:id', getUser)
-router.post('/logout', logOut)
-router.post('/login', getUserLogIn)
+//GET
+router.get('/users', getUsers);
+router.get('/users/:id', getUser);
+router.get('/users/rol/:id', getUserByRol);
+
+//POST
+router.post('/users', createUsers);
+//router.post('/login', getUserLogIn);
+router.post('/logout', logOut);
 //router.post('/register', getUserLogIn)
-router.post('/users', createUsers)
-router.put('/users/:id', updateUser)
-router.delete('/users/:id', deleteUser)
-router.get('/users/rol/:id', getUserByRol)
+
+//PUT
+router.put('/users/:id', updateUser);
+
+//DELETE
+router.delete('/users/:id', deleteUser);
 export default router
-
-// router.get('/users', (req,res)=>{
-//     res.send('Obteniendo usuarios')
-// })
-
-// router.get('/users/:id', (req,res)=>{
-//     const {id} = req.params;
-//     res.send('Obteniendo un usuario' + id)
-// })
-// router.post('/users', (req,res)=>{
-//     res.send('Creando usuarios')
-// })
-// router.put('/users/:id', (req,res)=>{
-//     const {id} = req.params;
-//     res.send('Actualizando un usuario' + id)
-// })
-
-// router.delete('/users/:id', (req,res)=>{
-//     const {id} = req.params;
-//     res.send('Eliminando un usuario' + id)
-// })
