be16b85de4bf4973ac3b7abef7be0637695c8a79
Matias Micchele <matias.micchele@hotmail.com>
2025-03-08 20:39:50 -0300

    refactorizacion de token y middlewares

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,12 +1,14 @@
 //* el .routes es para encontrar este archivo mas rapido, pero simplemente podría llamarse users.js
 import { Router } from 'express'
 import { getUsers, getUser, createUsers, updateUser, deleteUser, getUserByRol, login, logOut } from '../controllers/users.controllers.js'
+import { validationToken } from '../middlewares/validationToken.js';
 const router = Router()
 
 //GET
 router.get('/users', getUsers);
 router.get('/users/:id', getUser);
 router.get('/users/rol/:id', getUserByRol);
+router.get('/validateSession', validationToken); //Valida si la session esta activa desde el front
 
 //POST
 router.post('/users', createUsers);
