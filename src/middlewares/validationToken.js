37924f52686f0b1e900dff2d8e2621e376ef82c7
Matias Micchele <matias.micchele@hotmail.com>
2025-03-12 12:48:52 -0300

    cambios menores

--- src/middlewares/validationToken.js
+++ src/middlewares/validationToken.js
@@ -4,13 +4,17 @@ import { SECRET_JWT_KEY } from './../config.js';
 export const validationToken = (req, res, next) => {    
     const token = req.cookies.access_token;
 
+    console.log("aca en el validaToken");
+    console.log(token);
     //Agrego información a la sesión
     req.session = {user: null};
 
-    try {
+    try {        
        const data = jwt.verify(token, SECRET_JWT_KEY);
+       console.log("----------------------------");
+       console.log(data);
         req.session.user = data;
-        next() //seguir a la siguiente ruta o middleware
+        //next() //seguir a la siguiente ruta o middleware
     } catch (error) {        
         return res.status(401).json({message: 'Unauthorized'})                
     }
